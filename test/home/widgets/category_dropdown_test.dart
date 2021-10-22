99207808fd4369a13c4205ac20f02f69f3dcfe39
Lucas Alexandre Fell <lucas.fell@zallpy.com>
2021-10-22 18:20:54 -0300

    category dropdown test + 98% coverage

--- test/home/widgets/category_dropdown_test.dart
+++ test/home/widgets/category_dropdown_test.dart
@@ -1,35 +1,55 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:chuck_norris/home/bloc/bloc.dart';
-import 'package:chuck_norris/home/widgets/category_dropdown.dart';
+import 'package:chuck_norris/home/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockCategoryBloc extends MockBloc<CategoryEvent, CategoryState>
+    implements CategoryBloc {}
+
+class FakeCategoryEvent extends Fake implements CategoryEvent {}
+
+class FakeCategoryState extends Fake implements CategoryState {}
 
 main() {
+  late CategoryBloc categoryBloc;
   group('DropdownMenu', () {
     late Widget widgetUnderTest;
+
+    setUpAll(() {
+      registerFallbackValue(FakeCategoryEvent());
+      registerFallbackValue(FakeCategoryState());
+    });
+
     setUp(() {
-      widgetUnderTest = (MaterialApp(
+      categoryBloc = MockCategoryBloc();
+      widgetUnderTest = MaterialApp(
         home: Scaffold(
           body: BlocProvider(
-            create: (context) => CategoryBloc(),
+            create: (context) => categoryBloc,
             child: const CategoryDropdown(),
           ),
         ),
-      ));
+      );
     });
-    testWidgets('change joke category on tap', (widgetTester) async {
+    testWidgets('Updated category state', (widgetTester) async {
       try {
+        String category = 'dev';
+        when(() => categoryBloc.state)
+            .thenReturn(CategoryUpdated(category: category));
         await widgetTester.pumpWidget(widgetUnderTest);
         final dropdown = find.byKey(const Key('joke_category_dropdown'));
         await widgetTester.tap(dropdown);
         widgetTester.pumpAndSettle();
-        final dropdownItem = find.text('dev').last;
+        final dropdownItem = find.text(category).last;
         await widgetTester.dragUntilVisible(
             dropdownItem, dropdown, const Offset(0, 20));
         await widgetTester.pumpAndSettle();
         await widgetTester.tap(dropdownItem);
         await widgetTester.pumpAndSettle();
-        expect('dev', findsOneWidget);
+        expect(category, findsOneWidget);
       } catch (_) {}
     });
   });
